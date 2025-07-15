5802e2b3901f4587975d8cdab6d21de59164e9a2
Haochen Shen <haochenshen@mac.watson.ibm.com>
2025-07-15 13:23:30 -0400

    Added mamba_ssm to huggingface format conversion

---
+++ convert_fms_model_to_hf.py
@@ -0,0 +1,30 @@
+import subprocess
+from fms_to_hf_mamba_transformers import convert_mamba_ssm_checkpoint_file_to_huggingface_model_file
+from fms_to_hf_mamba import main as fms_to_hf
+
+import glob
+import shutil
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src-dir', type=str, required=True, help="Model source directory, (example: /gpfs/davis/bamba_tune/zloss-500k-step-128k/checkpoints/step_6000_ckp)")
+    parser.add_argument('--model-name', type=str, required=True, help="Model name, (example: zloss-500k-step-128k)")
+    parser.add_argument('--model-dir', type=str, required=True, help="Saving directory")
+    parser.add_argument('--model-type', type=str, required=True, help="Mamba model type, (example: mamba_9.8b)")
+
+    args = parser.parse_args()
+
+    DEST_DIR = args.model_dir + '/' + args.model_name
+    TOKENIZER_DIR = '/datasets/tokenizers/llama3'
+
+    # --src-dir=/gpfs/hshen/bamba_upi_tune/bamba_upi_32k_layer/pth/step_6000/consolidated.00.pth
+    fms_to_hf(args.model_type, args.src_dir, DEST_DIR, TOKENIZER_DIR)
+
+    convert_mamba_ssm_checkpoint_file_to_huggingface_model_file(
+        DEST_DIR , 'fp32', DEST_DIR + '/hf', save_model='sharded'
+    )
+
+    for file in glob.glob(DEST_DIR + '/*token*'):
+        shutil.copy2(file, DEST_DIR + '/hf/')
