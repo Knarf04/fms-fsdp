1d7bcbbcfc4121fb5756d5af177590773f6da245
Haochen Shen <haochenshen@mac.watson.ibm.com>
2025-08-06 14:58:08 -0400

    Update State passing dict content: batch size

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -338,7 +338,7 @@ def get_model_config(model_variant):
             "pad_vocab_size_multiple": 16,
             "tie_embeddings": False,
             "experiments": {
-                "sp": "",
+                "sp": 4,
             }
         }
     else:
