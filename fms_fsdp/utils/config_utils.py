ec68a160415f43326bab87e6a4cd8e9a64ce1cfb
Haochen Shen <shenhaochen2000@gmail.com>
2026-03-12 05:48:39 -0500

    Add config for YaRN and fixed budget

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -1012,6 +1012,32 @@ def get_model_config(model_variant):
             prune=True,
             prune_thresh=float(1/2),
         )
+    elif model_variant == "llama_1b_yarn":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=4096,
+            rope_theta=500000.0,
+            rope_scaling={"rope_type": "yarn"},
+            prune=False,
+        )
+    elif model_variant == "llama_1b_fixed":
+        model_config = LLaMAConfig(
+            src_vocab_size=128256,
+            emb_dim=1280,
+            nheads=16,
+            kvheads=4,
+            nlayers=32,
+            hidden_grow_factor=3.2,
+            max_expected_seq_len=4096,
+            rope_theta=500000.0,
+            prune=True,
+            prune_topk=256,
+        )
     else:
         raise ValueError(f"model variant {model_variant} not supported.")
 
